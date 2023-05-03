47791c9175b6df42bda0f7eb788ee367b9e09585
ihalangel <ihalangel@gmail.com>
2023-05-03 06:38:58 -0400

    evitando lectura null de compra

--- src/script/processor/index.js
+++ src/script/processor/index.js
@@ -21,12 +21,12 @@ async function displayHello() {
 
  const venta_a_procesar= await despacho.get_ph()
  if(venta_a_procesar.length>=1){
-    console.log("Ventas H-pay por entregar", venta_a_procesar.length); 
-
-    console.log("Venta hivepay a procesar", venta_a_procesar);
-    await despacho.entregar_venta(venta_a_procesar)
-
     console.log("AQUI SE PUEDE AGREGAR EL PEO PARA ENTREGAR EL NFT COMPRADO")
+    console.log("Ventas H-pay por entregar", venta_a_procesar.length); 
+    console.log("Venta hivepay a procesar", venta_a_procesar);
+  //  await despacho.entregar_venta(venta_a_procesar)
+
+   
 }
 
 const venta_block_a_procesar = await despacho.get_pb()

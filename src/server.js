6dcc54c12563d5bbdae4577fb13f47b1c78ac190
ihalangel <ihalangel@gmail.com>
2024-06-09 05:11:28 +0000

    server nuevo

--- src/server.js
+++ src/server.js
@@ -86,44 +86,6 @@
 
 
 
-// require("dotenv").config();
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const router = require('./network/routes');
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// app.use(bodyParser.json());
-// router(app);
-
-// // Ruta para servir archivos estáticos en modo de producción
-// if (process.env.NODE_ENV === "production") {
-//   // Ajustar la ruta para que apunte al directorio build en el nivel superior
-//   app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
-//   // Manejar todas las demás rutas y enviar al archivo index.html
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'), (err) => {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-//     });
-//   });
-// }
-
-// app.listen(port, (err) => {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log(`Example app listening on port ${port}`);
-// });
-
-
-
-
-
 require("dotenv").config();
 const express = require('express');
 const path = require('path');

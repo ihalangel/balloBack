89263470e306ce78c831fcb95ccc872400221ed6
ihalangel <ihalangel@gmail.com>
2024-06-09 13:34:42 +0000

    nuevo

--- src/server.js
+++ src/server.js
@@ -41,45 +41,45 @@
 
 
 
-require("dotenv").config();
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = require('./network/routes');
+// require("dotenv").config();
+// const express = require('express');
+// const path = require('path');
+// const bodyParser = require('body-parser');
+// const router = require('./network/routes');
 
-const app = express();
-const port = process.env.PORT || 4000;
+// const app = express();
+// const port = process.env.PORT || 4000;
 
-app.use(bodyParser.json());
-router(app);
+// app.use(bodyParser.json());
+// router(app);
 
-// Ruta para servir archivos estáticos en modo de producción
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static('../build'));
+// // Ruta para servir archivos estáticos en modo de producción
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static('build'));
 
-  // Manejar todas las demás rutas y enviar al archivo index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
-
-
-
-}
+//   // Manejar todas las demás rutas y enviar al archivo index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'build', 'index.html'), (err) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
 
 
 
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(`Example app listening on port ${port}`);
-});
+// }
+
+
+
+
+// app.listen(port, (err) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log(`Example app listening on port ${port}`);
+// });
 
 
 
@@ -119,3 +119,40 @@ app.listen(port, (err) => {
 //   }
 //   console.log(`Example app listening on port ${port}`);
 // });
+
+
+
+
+
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = require('./network/routes');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+router(app);
+
+// Ruta para servir archivos estáticos en modo de producción
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../build')));
+
+  // Manejar todas las demás rutas y enviar al archivo index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'), (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Example app listening on port ${port}`);
+});
